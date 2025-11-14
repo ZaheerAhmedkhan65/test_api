fc02a2fe4f136545ae95574cd7483968d3c5397a
ZaheerAhmedkhan65 <ranazaheerahmed65@gmail.com>
2025-11-14 14:15:00 +0500

    updated connector db connection

--- api/db.py
+++ api/db.py
@@ -10,15 +10,19 @@ dbconfig = {
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASS"),
     "database": os.getenv("DB_NAME"),
-    "port": os.getenv("DB_PORT")
+    "port": int(os.getenv("DB_PORT", 4000)),  # TiDB Cloud default MySQL port is usually 4000
+    "ssl_disabled": False,                    # enable SSL
+    "ssl_verify_cert": False                  # same as rejectUnauthorized: false
 }
 
+# Connection pool
 connection_pool = pooling.MySQLConnectionPool(
-    pool_name="product_pool",
-    pool_size=5,
+    pool_name="tidb_pool",
+    pool_size=10,
     pool_reset_session=True,
     **dbconfig
 )
 
 def get_connection():
+    """Get a connection from the TiDB pool"""
     return connection_pool.get_connection()
